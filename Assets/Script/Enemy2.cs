aa3c769269302be5a4403f8c52fa2c595437adcd
Dwuds <dawfw7399@gmail.com>
2026-02-26 21:18:22 +0700

    Script untuk Enemy 2

--- Assets/Script/Enemy2.cs
+++ Assets/Script/Enemy2.cs
@@ -2,15 +2,91 @@ using UnityEngine;
 
 public class Enemy2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Transform player;
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public float moveSpeed = 6f;
+    public float hoverDuration = 1.5f;
+    public float verticalAdjustSpeed = 5f;
+
+    private float hoverTimer;
+    private bool isCharging = false;
+    private float lockedY;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        hoverTimer = hoverDuration;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isCharging)
+        {
+            HoverPhase();
+        }
+        else
+        {
+            ChargePhase();
+        }
+    }
+
+    void HoverPhase()
+    {
+        hoverTimer -= Time.deltaTime;
+
+        if (hoverTimer <= 0f)
+        {
+            LockPlayerPosition();
+            isCharging = true;
+        }
+    }
+
+    void LockPlayerPosition()
+    {
+        if (player != null)
+        {
+            lockedY = player.position.y;
+        }
+        else
+        {
+            lockedY = transform.position.y;
+        }
+    }
+
+    void ChargePhase()
+    {
+        // Gerak ke kiri
+        float newX = transform.position.x - moveSpeed * Time.deltaTime;
+
+        // Menuju posisi Y yang sudah di-lock
+        float newY = Mathf.MoveTowards(
+            transform.position.y,
+            lockedY,
+            verticalAdjustSpeed * Time.deltaTime
+        );
+
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
